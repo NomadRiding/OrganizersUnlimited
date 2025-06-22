5a7bef491e817df001a1d707bc4014e430121f22
nomadriding <fundora.a@gmail.com>
2025-06-22 09:16:04 -0400

    prep files

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "./HomePage.css"
+import Layout from "../Components/Layout"
+
+function HomePage() {
+  return (
+    <>
+      <Layout>
+        <div className="home-page">
+          <h1>Welcome to Organizers Unlimited</h1>
+          <p>Your one-stop solution for all your organizing needs!</p>
+          <div className="features">
+            <h2>Features</h2>
+            <ul>
+              <li>Customizable organizers</li>
+              <li>Easy-to-use interface</li>
+              <li>Affordable pricing</li>
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </>
+  )
+}
